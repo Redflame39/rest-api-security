3874652b00d99be89e0cc59519abe620f4fdc52c
Redflame39 <vanya133719@gmail.com>
2022-03-12 14:01:22 +0300

    Initial commit

---
+++ src/test/java/com/epam/esm/restapisecurity/RestApiSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.esm.restapisecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
